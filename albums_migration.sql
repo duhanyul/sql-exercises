5f77785a50d83863cb3fb8c59070814077390285
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-08-07 15:14:20 -0500

    increased number of chars on genre column

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,12 @@
 use codeup_test_db;
-create table albums(
+drop table if exists albums;
+
+create table if not exists albums(
   id int unsigned not null auto_increment,
   artist varchar(150) default 'none',
   name varchar(150) not null,
   release_date year(4),
   sales float(4,2),
-  genre varchar(25),
+  genre varchar(128),
   primary key (id)
 );
