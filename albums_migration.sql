65cb7a9b0a4360cc688f400a35bb44d03d2d9ea5
Daniel Rojo-Gutierrez <danielrojo-gutierrez@Daniels-MacBook-Pro.local>
2017-08-08 11:10:15 -0500

    added completed update exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -5,8 +5,8 @@ create table if not exists albums(
   id int unsigned not null auto_increment,
   artist varchar(150) default 'none',
   name varchar(150) not null,
-  release_date year(4),
-  sales float(4,2),
+  release_date int(4),
+  sales float(100,2),
   genre varchar(128),
   primary key (id)
 );
